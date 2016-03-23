13cb20871b20f41744d1b2e831bbcdb6e778675f
zz <zz.thoreau@gmail.com>
2016-03-23 17:52:56 +0800

    spring mvc learn

---
+++ springmvc/src/main/java/com/mvc/henry/entity/User.java
@@ -0,0 +1,25 @@
+package com.mvc.henry.entity;
+
+public class User {
+	private String username;
+	private String password;
+	private String addres;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddres() {
+		return addres;
+	}
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+}
