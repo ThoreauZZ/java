c39fb2771a735198db01f160355349bc8e0f6954
zz <zz.thoreau@gmail.com>
2016-04-24 22:19:18 +0800

    update

---
+++ springmvc/src/main/java/com/mvc/henry/web/controller/BaseController.java
@@ -0,0 +1,14 @@
+package com.mvc.henry.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * henry.zhao
+ * Created by henry.zhao on 2016/4/13.
+ */
+@Controller
+public class BaseController {
+
+}
