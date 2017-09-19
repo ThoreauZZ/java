97cee3c5c5bc31761243f269fb707144255d0bd7
zhaozhou <zz.thoreau@gmail.com>
2017-09-19 11:07:41 +0800

    algorithm

---
+++ algorithm/src/main/java/com/thoreau/App.java
@@ -0,0 +1,16 @@
+package com.thoreau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int n = 100;
+        for(int j=1; j<=n; j*=2) {
+            System.out.println(j);
+        }
+    }
+}
