3d266e0bcf6184a72d5a50067fa4953d283559a5
zhaozhou <zz.thoreau@gmail.com>
2018-02-05 20:58:44 +0800

    update  test

--- algorithm/src/test/java/com/thoreau/algorithm/cache/LruCacheTest.java
+++ algorithm/src/test/java/com/thoreau/algorithm/cache/LruCacheTest.java
@@ -1,6 +1,17 @@
 package com.thoreau.algorithm.cache;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
 import org.junit.Test;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -33,10 +44,11 @@ public class LruCacheTest {
         assertEquals(new Integer(1), lruCache.get(1));
         lruCache.put(3, 3);
         assertNull(lruCache.get(2));
-        lruCache.put(4,4);
+        lruCache.put(4, 4);
         assertNull(lruCache.get(1));
-        assertEquals(new Integer(3),lruCache.get(3));
+        assertEquals(new Integer(3), lruCache.get(3));
     }
+
     @Test
     public void testLRUCacheHashMapAndDoublyLinked() {
         LruCache<Integer, Integer> lruCache = new LRUCacheHashMapAndDoublyLinked<>(2);
@@ -45,8 +57,55 @@ public class LruCacheTest {
         assertEquals(new Integer(1), lruCache.get(1));
         lruCache.put(3, 3);
         assertNull(lruCache.get(2));
-        lruCache.put(4,4);
+        lruCache.put(4, 4);
         assertNull(lruCache.get(1));
-        assertEquals(new Integer(3),lruCache.get(3));
+        assertEquals(new Integer(3), lruCache.get(3));
+    }
+
+    @Test
+    public void testGuava() throws ExecutionException, InterruptedException {
+        LoadingCache<String, String> cache = CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(2, TimeUnit.SECONDS)
+                .removalListener((RemovalListener<String, String>) removalNotification -> System.out.println("Remove key " + removalNotification.getKey() + " ..."))
+                .build(new CacheLoader<String, String>() {
+                    public String load(String key) {
+                        return key + "-value";
+                    }
+                });
+        for (int i = 0; i < 20; i++) {
+            cache.put("key" + i, "vaue" + i);
+        }
+        System.out.println(cache.get("key19"));
+        System.out.println("--------------");
+        Thread.sleep(3000);
+        System.out.println(cache.get("key19"));
+    }
+
+    @Test
+    public void testReferenceQueue() {
+        ReferenceQueue<byte[]> referenceQueue = new ReferenceQueue<>();
+        Object value = new Object();
+        Map<Object, Object> map = new HashMap<>();
+        for (int i = 0; i < 10000; i++) {
+            byte[] bytes = new byte[1048576];
+            WeakReference<byte[]> weakReference = new WeakReference<byte[]>(bytes, referenceQueue);
+            map.put(weakReference, value);
+        }
+        Thread thread = new Thread(() -> {
+            try {
+                int cnt = 0;
+                WeakReference<byte[]> k;
+                while ((k = (WeakReference) referenceQueue.remove()) != null) {
+                    System.out.println((cnt++) + "回收了:" + k);
+                }
+            } catch (InterruptedException e) {
+            } finally {
+                System.out.println("结束循环");
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+        System.out.println("map.size->" + map.size());
     }
 }
