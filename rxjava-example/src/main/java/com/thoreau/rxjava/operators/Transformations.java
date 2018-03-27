1fe82b3c4febe75a9bbf525d8bcc449740464654
zhaozhou <zz.thoreau@gmail.com>
2018-03-27 19:21:19 +0800

    update rxjava

--- rxjava-example/src/main/java/com/thoreau/rxjava/operators/Transformations.java
+++ rxjava-example/src/main/java/com/thoreau/rxjava/operators/Transformations.java
@@ -1,5 +1,6 @@
 package com.thoreau.rxjava.operators;
 
+import com.thoreau.rxjava.base.Log;
 import org.junit.Test;
 import rx.Observable;
 
@@ -16,11 +17,12 @@ public class Transformations {
                   .subscribe(System.out::println);
 
         Observable.just(8, 9, 10)
-                .filter(i -> i % 3 > 0)
-                .map(i -> "#" + i * 10)
-                .filter(s -> s.length() < 4)
-                .subscribe(System.out::println);
+                  .filter(i -> i % 3 > 0)
+                  .map(i -> "#" + i * 10)
+                  .filter(s -> s.length() < 4)
+                  .subscribe(System.out::println);
 
+        System.out.println("-----------------");
         Observable
                 .just(8, 9, 10)
                 .doOnNext(i -> System.out.println("A: " + i))
@@ -30,5 +32,24 @@ public class Transformations {
                 .doOnNext(s -> System.out.println("C: " + s))
                 .filter(s -> s.length() < 4)
                 .subscribe(s -> System.out.println("D: " + s));
+        // 订阅up到最前，然后一个一个数据处理
+    }
+
+    @Test
+    public void flatMapTest() {
+        //transformation of each element can return another Observable
+        Observable.just(1, 2, 3)
+                  .flatMap(x -> Observable.just(x * 2))
+                  .subscribe(System.out::println);
+
+        System.out.println("------------------------------------");
+
+        Observable.just(1, 2, 3)
+                  .flatMap(x -> Observable.create(subscriber -> {
+                      Log.info("falt-->"+x + " x 2");
+                      subscriber.onNext(x * 2);
+                  }))
+                  .subscribe(System.out::println);
+
     }
 }
