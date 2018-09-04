a49e53aabda698b04aaa3164c31bafeae2f07ffc
thoreau <zz.thoreau@Gmail.com>
2018-09-05 00:27:12 +0800

    add docker for sprin boot kotlin

--- spring-boot-kotlin/src/main/java/com/thoreau/boot/Greeting.kt
+++ spring-boot-kotlin/src/main/java/com/thoreau/boot/Greeting.kt
@@ -1,7 +1,9 @@
 package com.thoreau.boot
 
+import java.time.LocalDateTime
+
 /**
  * 2018/9/3 下午11:14.
  * @author zhaozhou
  */
-data class Greeting(val id: Long, val content: String, val ip: String, val address: String)
+data class Greeting(val id: Long, val content: String, val ip: String, val address: String,val time: String)
--- spring-boot-kotlin/src/main/java/com/thoreau/boot/GreetingController.kt
+++ spring-boot-kotlin/src/main/java/com/thoreau/boot/GreetingController.kt
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.net.InetAddress
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.atomic.AtomicLong
-
 
 
 /**
@@ -17,6 +18,14 @@ class GreetingController {
     val counter = AtomicLong()
     var inetAddress = InetAddress.getLocalHost()
     @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name",inetAddress.hostAddress,inetAddress.hostName)
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) : Greeting{
+        val formater = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        val time = LocalDateTime.now().format(formater);
+        return Greeting(counter.incrementAndGet(),
+                "Hello, $name", inetAddress.hostAddress
+                , inetAddress.hostName
+                , time)
+
+    }
+
 }
