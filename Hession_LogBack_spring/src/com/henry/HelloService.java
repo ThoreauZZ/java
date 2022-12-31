a64bff18d475c242a0d94c8b577542406774640f
thoreau <zhaozhou@zoeMacbook.local>
2022-12-31 13:45:57 +0800

    rm old code

--- Hession_LogBack_spring/src/com/henry/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.henry;
-/*
- * 定义hessian的接口和方法
- */
-public interface HelloService {
-	public String sayHello();
-
-}
--- zookeeper_demo/src/main/java/com/henry/zk/publisher/ConfigChangeListener.java
+++
@@ -1,5 +0,0 @@
-package com.henry.zk.publisher;
-
-public interface ConfigChangeListener {
-	public abstract void configChanged(String paramString1, String paramString2);
-}
